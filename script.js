954f395b3e359d556d83af13bf31f3e2284dd741
Edward Omopariola <femiedjr@gmail.com>
2024-09-16 15:03:08 -0400

    changes on html, css, js

--- script.js
+++ script.js
@@ -1,25 +1,39 @@
 const minNum = 1
 const maxNum = 100
+let attempt = 0
 let btn = document.getElementById("btn");
 let output = document.getElementById("output");
-let resetBtn = document.getElementById("Reset");
-let randomNum = Math.floor(Math.random() * maxNum);
+let resetBtn = document.getElementById("resetBtn");
+let randomNum = Math.floor(Math.random() * maxNum)+ 1
+console.log(randomNum);
+console.log('attempt', attempt);
 
 
 btn.addEventListener('click', function() {
+    
     let input = document.getElementById("userInput").value;
 
-    if (input < randomNum) {
-        output.innerHTML = "Number too low, please try again"
-    };
-    if (input > randomNum) {
-        output.innerHTML = "Number too high, Please try again"
-    };
-    if (input == randomNum) {
-        output.innerHTML = "Congratulation you guess the correct number"
-    }    
+        if (input < randomNum) {
+            output.innerHTML = "Number too low, please try again"
+            attempt++
+        };
+        if (input > randomNum) {
+            output.innerHTML = "Number too high, Please try again"
+            attempt++
+        };
+        if (input == randomNum) {
+            output.innerHTML = "Congratulation you guess the correct number"
+            attempt = 0;
+        };
+        if (attempt > 3) {
+            output.innerHTML = `You've reach your limit, ${randomNum}. was the correct answer. click the reset button to start again`
+        };
+       
 });
 
-// resetBtn.addEventListener("click", function() {
-//     Reset = 0;
-// });
+resetBtn.addEventListener('click', function() {
+    attempt = 0;
+    output.innerHTML = "Enter a number";
+    let input = document.getElementById("userInput").value;
+});
+
